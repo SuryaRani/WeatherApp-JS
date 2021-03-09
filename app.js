06fa71d970e8f1ff67b767a30b2198a4cd14a1c4
Surya Rani <srani@ucsc.edu>
2021-03-08 23:11:16 -0800

    added click ability to change between Farenheit and Celsius

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ let lat;
 let tempDegree = document.querySelector(".temp-degree");
 let tempDescript = document.querySelector(".temp-description");
 let locationTimezone = document.querySelector(".location-timezone");
+let tempArea = document.querySelector(".temperature");
+let ForC = document.querySelector(".temperature span");
 
 if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(pos => {
@@ -20,9 +22,38 @@ if (navigator.geolocation) {
             })
             .then(data => {
                 console.log(data);
+                setContent(data);
+
             });
     });
+}
 
-
+function setContent(data) {
+    tempDegree.textContent = Math.round((data.main.temp - 273) * (9 / 5) + 32);
+    tempDescript.textContent = toTitleCase(data.weather[0].description);
+    locationTimezone.textContent = data.name;
+    setIcon(tempDescript.textContent);
 
 }
+
+function setIcon(descript) {
+
+}
+
+
+function toTitleCase(str) {
+    return str.replace(/\w\S*/g, function (txt) {
+        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    });
+}
+
+tempArea.addEventListener("click", function () {
+    if (ForC.textContent === "F") {
+        ForC.textContent = "C";
+        tempDegree.textContent = Math.round((tempDegree.textContent - 32) * (5 / 9));
+    }
+    else {
+        ForC.textContent = "F";
+        tempDegree.textContent = Math.round((tempDegree.textContent * (9 / 5) + 32));
+    }
+});
